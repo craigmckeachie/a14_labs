177d99977ceb244c65de345d4eb39940fa8c27b3
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-14 17:38:37 -0400

    lab32

--- src/app/projects/projects-container/projects-container.component.ts
+++ src/app/projects/projects-container/projects-container.component.ts
@@ -1,26 +1,41 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { Project } from '../shared/project.model';
-import { ProjectService } from '../shared/project.service';
-import { Subject, Observable, Subscription } from 'rxjs';
+import { Subject, Subscription, of } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { select, Store } from '@ngrx/store';
+import { State } from 'src/app/reducers';
+import {
+  getError,
+  getLoading,
+  getProjects,
+  getSaving,
+} from '../shared/state/project.reducer';
+import { load, save } from '../shared/state/project.actions';
 
 @Component({
   selector: 'app-projects-container',
   templateUrl: './projects-container.component.html',
   styleUrls: ['./projects-container.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsContainerComponent implements OnInit, OnDestroy {
-  projects: Project[] = [];
-  errorMessage: string = '';
-  loading: boolean = false;
+  projects$ = this.store.pipe(select(getProjects));
+  errorMessage$ = this.store.pipe(select(getError));
+  loading$ = this.store.pipe(select(getLoading));
+  saving$ = this.store.pipe(select(getSaving));
   private searchTerms = new Subject<string>();
   private subscription!: Subscription;
 
-  constructor(private projectService: ProjectService) {}
+  constructor(private store: Store<State>) {}
 
   ngOnInit() {
     this.observeSearchTerms();
-    this.searchTerms.next('');
+    this.store.dispatch(load({ name: '' }));
   }
 
   onSearch(term: string) {
@@ -37,37 +52,20 @@ export class ProjectsContainerComponent implements OnInit, OnDestroy {
         distinctUntilChanged(),
 
         // switch to new search observable each time the term changes
-        switchMap((term: string): Observable<Project[]> => {
-          this.loading = true;
-          return this.projectService.listByName(term);
+        switchMap((term: string) => {
+          this.store.dispatch(load({ name: term }));
+          return of(term);
         })
       )
-      .subscribe(
-        (data) => {
-          this.loading = false;
-          this.projects = data;
-        },
-        (error) => {
-          this.loading = false;
-          this.errorMessage = error;
-        }
-      );
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+      .subscribe();
   }
 
   onSaveListItem(event: any) {
     const project: Project = event.item;
-    this.projectService.put(project).subscribe(
-      (updatedProject) => {
-        const index = this.projects.findIndex(
-          (element) => element.id === project.id
-        );
-        this.projects[index] = project;
-      },
-      (error) => (this.errorMessage = error)
-    );
+    this.store.dispatch(save({ project }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
